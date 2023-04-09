262bbdbcb578ed5c79194c059fba76dad9642a55
Max Clinton <maxtclinton@gmail.com>
2023-04-08 22:58:04 -0400

    Adjust file path

--- src/main.rs
+++ src/main.rs
@@ -18,32 +18,6 @@ use std::fs;
 use std::io;
 mod pokeball;
 
-
-// fn show_sprite(sprite: &str, poke_type: &str) {
-//     let poke_colors = HashMap::from([
-//         ("normal", (168, 167, 122)),
-//         ("fire", (238, 129, 48)),
-//         ("water", (99, 144, 240)),
-//         ("electric", (247, 208, 44)),
-//         ("grass", (122, 199, 76)),
-//         ("ice", (150, 217, 214)),
-//         ("fighting", (194, 46, 40)),
-//         ("poison", (163, 62, 161)),
-//         ("ground", (226, 191, 101)),
-//         ("flying", (169, 143, 243)),
-//         ("psychic", (249, 85, 135)),
-//         ("bug", (166, 185, 26)),
-//         ("rock", (182, 161, 54)),
-//         ("ghost", (115, 87, 151)),
-//         ("dragon", (111, 53, 252)),
-//         ("dark", (112, 87, 70)),
-//         ("steel", (183, 183, 206)),
-//         ("fairy", (214, 133, 173)),
-//     ]);
-//     let (r, g, b) = poke_colors.get(poke_type).unwrap();
-//     println!("{}", sprite.truecolor(*r, *g, *b));
-// }
-
 fn initialize_pokemon() {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let mut connection = PgConnection::establish(&database_url)
@@ -94,7 +68,7 @@ fn main() {
             .load::<Pokemon>(&mut connection)
             .expect("Error loading posts");
         if pokemon_result.len() > 0 {
-            let file_path = format!("sprites/large/regular/{}", pokemon_result[0].name);
+            let file_path = format!("sprites/large/{}", pokemon_result[0].name);
             let sprite = fs::read_to_string(file_path).expect("Should have been able to read the sprite file");
             println!("{}", sprite);
             println!("{}", pokemon_result[0].name);
