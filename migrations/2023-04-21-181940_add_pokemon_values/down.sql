e494d5e98fda4e734c8c665dbbcdf5f1604b5b2f
Max Clinton <maxtclinton@gmail.com>
2023-04-21 14:23:40 -0400

    Add pokemon values migration

---
+++ migrations/2023-04-21-181940_add_pokemon_values/down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE pokemon
+DROP COLUMN base_experience INT;
+ALTER TABLE pokemon
+DROP COLUMN height INT;
+ALTER TABLE pokemon
+DROP COLUMN weight INT;
---
+++ migrations/2023-04-21-181940_add_pokemon_values/up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE pokemon
+ADD COLUMN base_experience INT;
+ALTER TABLE pokemon
+ADD COLUMN height INT;
+ALTER TABLE pokemon
+ADD COLUMN weight INT;
