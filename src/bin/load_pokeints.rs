84ba190f1e87511d77b2c58417330cf0c888d247
Max Clinton <maxtclinton@gmail.com>
2023-04-21 22:04:48 -0400

    Add script to load new pokemon values

---
+++ src/bin/load_pokeints.rs
@@ -0,0 +1,146 @@
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::io;
+extern crate termdex;
+use serde_json::Value;
+use termdex::models::*;
+use termdex::schema::pokemon::dsl::pokemon;
+use termdex::schema::pokemon::large;
+use termdex::schema::pokemon::pokemon_id;
+use termdex::schema::pokemon::small;
+
+
+#[derive(Deserialize)]
+pub struct PokemonAPIData {
+    pub name: String,
+    pub types: Vec<PokeType>,
+    pub stats: Vec<Stat>,
+    pub abilities: Vec<PokeAbility>,
+    pub base_experience: u64,
+    pub height: u64,
+    pub moves: Vec<PokeMove>,
+    pub weight: u64,
+}
+
+#[derive(Deserialize)]
+pub struct PokeAbility {
+    pub ability: PokeAbilityName,
+}
+
+#[derive(Deserialize)]
+pub struct PokeAbilityName {
+    pub name: String,
+}
+
+#[derive(Deserialize)]
+pub struct PokeMove {
+    pub r#move: PokeMoveDetails,
+}
+
+#[derive(Deserialize)]
+pub struct PokeMoveDetails {
+    pub name: String,
+    pub url: String,
+}
+
+#[derive(Deserialize)]
+pub struct PokeType {
+    #[serde(rename = "type")]
+    pub poketype: TypeName,
+}
+
+#[derive(Deserialize)]
+pub struct TypeName {
+    pub name: String,
+}
+
+#[derive(Deserialize)]
+pub struct Stat {
+    base_stat: u64,
+    effort: u64,
+    stat: StatName,
+}
+
+#[derive(Deserialize)]
+pub struct StatName {
+    name: String,
+}
+
+///A Downloader to download web content
+pub struct Downloader {
+    client: reqwest::blocking::Client,
+    tries: usize,
+}
+
+impl Downloader {
+    /// Create a new Downloader
+    pub fn new(tries: usize, user_agent: &str) -> Downloader {
+        Downloader {
+            client: reqwest::blocking::ClientBuilder::new()
+                .cookie_store(true)
+                .user_agent(user_agent)
+                .build()
+                .unwrap(),
+            tries,
+        }
+    }
+
+    ///Download the content at this url
+    fn make_request(&self, url: &str) -> Result<PokemonAPIData, reqwest::Error> {
+        let req = self.client.get(url);
+        match req.send() {
+            Ok(mut response) => {
+                let pokemon: PokemonAPIData = response.json().unwrap();
+                Ok(pokemon)
+            }
+
+            Err(e) => {
+                println!("Downloader.get() has encountered an error: {}", e);
+                Err(e)
+            }
+        }
+    }
+
+    ///Download the content of an url and retries at most 'tries' times on failure
+    pub fn get(&self, url: &str) -> Result<PokemonAPIData, reqwest::Error> {
+        let mut error: Option<reqwest::Error> = None;
+        for _ in 0..self.tries {
+            match self.make_request(url) {
+                Ok(response) => return Ok(response),
+                Err(e) => error = Some(e),
+            }
+        }
+
+        Err(error.unwrap())
+    }
+}
+
+
+fn main() {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let mut connection = PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    let pokemon_db_data = pokemon
+        .load::<Pokemon>(&mut connection)
+        .expect("Error loading pokemon");
+
+    let downloader = Downloader::new(3, "termdex");
+
+    for p in pokemon_db_data.iter() {
+        let url = format!("https://pokeapi.co/api/v2/pokemon/{}", p.name);
+        match downloader.get(&url) {
+            Ok(response) => {
+                println!("{:#?}", response.name);
+            }
+            Err(e) => {
+                println!("Couldn't download a page, {:?}", e);
+            }
+        }
+        println!("{}", p.name);
+    }
+}
